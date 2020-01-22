a8fc0af9e78757c7fb6a54ecb0e2e1604ad17553
zjf <1512797681@qq.com>
2020-01-22 13:30:59 +0800

    完成全屏放大缩小功能

--- src/components/basic-layout/header-main/index.js
+++ src/components/basic-layout/header-main/index.js
@@ -1,15 +1,45 @@
-import React,{Component} from 'react';
-import {Button, Icon} from 'antd';
+import React, { Component } from 'react';
+import { Button, Icon } from 'antd';
+import screenfull from 'screenfull';
 
 import './index.less';
 
 
-export default class HeaderMain extends Component{
-    render(){
+export default class HeaderMain extends Component {
+    state = {
+        isScreenfull: false
+    }
+
+    // 切换全屏图标
+    handleScreenFullChange = () => {
+        this.setState({
+            isScreenfull: !this.state.isScreenfull
+        })
+    }
+
+    // 监听切换事件
+    componentDidMount() {
+        screenfull.on('change', this.handleScreenFullChange);
+    }
+
+    // 解绑监听事件
+    componentWillUnmount() {
+        screenfull.off('change', this.handleScreenFullChange);
+    }
+    
+    // 切换全屏/缩小全屏
+    screenFull = () => {
+        screenfull.toggle();
+    }
+
+    render() {
+        const { isScreenfull } = this.state
         return (
             <div className="header-main">
                 <div className="header-main-top">
-                    <Button size="small"><Icon type="fullscreen" /></Button>
+                    <Button size="small" onClick={this.screenFull}>
+                        <Icon type={isScreenfull ? "fullscreen-exit" : "fullscreen"} />
+                    </Button>
                     <Button size="small" className="header-main-lang">English</Button>
                     <span>hello</span>
                     <Button size="small" type="link">退出</Button>
